40c8ed66f35e2aa3d1d34a845f2c8263fc9bb3f4
Edivan Götz <edivangotz@gmail.com>
2026-01-23 21:53:51 -0300

    Definição das rotas do calendário

--- src/routes/calendarRoutes.js
+++ src/routes/calendarRoutes.js
@@ -5,5 +5,17 @@ const calendarController = require('../controllers/calendarController');
 
 const router = express.Router();
 
-// Rota para listar todos os eventos do calendário
+// Rota para listar todos os calendários
 router.get('/', calendarController.getAll);
+
+// Rota para criar um novo calendário
+router.post('/', calendarController.create);
+
+// Rota para atualizar um calendário existente
+router.put('/:id_calendario', calendarController.update);
+
+// Rota para deletar um calendário
+router.delete('/:id_calendario', calendarController.delete);
+
+module.exports = router;
+// Exporta o roteador para ser usado na aplicação principal
